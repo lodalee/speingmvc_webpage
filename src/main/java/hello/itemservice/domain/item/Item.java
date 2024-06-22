dc650a53a0f6f6a98ab3cabc101dbf9cfa7c71ef
chaewon <cwlee4633@naver.com>
2024-06-23 02:36:57 +0900

    상품 도메인 개발

---
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -0,0 +1,25 @@
+package hello.itemservice.domain.item;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class Item {
+
+    private Long id;
+    private String itemName;
+
+    //가격이나 수량이 들어가지 않을 수도 있기 때문에 Integer 를 썼다.
+    private Integer price;
+    private Integer quantity;
+
+    public Item() {
+
+    }
+
+    public Item(String itemName, Integer price, Integer quantity) {
+        this.itemName = itemName;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
